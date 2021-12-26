85b828d0f6486c0161bfb2c08351eda6b437d0fe
LIgorZ <96433539+LIgorZ@users.noreply.github.com>
2021-12-26 16:24:11 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace VectorAndPolynomial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("NET.C#.05 Инакапсуляция данных и методов");
+            Console.WriteLine("Задание 1.Векторы");
+            Vector a = new Vector(0, 1, 1);
+            Vector b = new Vector(0, 1, 3);
+
+            Console.WriteLine("Дано: вектор а{0} и вектор b{1}", a.ToString(), b.ToString());
+
+            // Длина векторов
+            // Свойства
+            Console.WriteLine("Длина векторв a: {0}", a.Length);
+            Console.WriteLine("Длина векторв b: {0}", b.Length);
+            // Методы
+            //Console.WriteLine("Длина векторв a: {0}", a.Length());
+            //Console.WriteLine("Длина векторв b: {0}", b.Length());
+
+            // Угол между векторам (cos)
+            Console.WriteLine("Угол между вектором a и b, градусы: {0}", a.Angle(b));
+
+            // Сложение векторов
+            Console.WriteLine("a{0} + b{1} = c{2}", a.ToString(), b.ToString(), a.Plus(b));
+            Console.WriteLine("a{0} + b{1} = c{2}", a.ToString(), b.ToString(), a + b);
+
+            // Умножение вектора на число
+            Console.WriteLine("{0} * a{1} = c{2}", 2, a.ToString(), a.SubDigit(2));
+            Console.WriteLine("{0} * a{1} = c{2}", 2, a.ToString(), 2 * a);
+
+            // Скалярное умножение
+            Console.WriteLine("a{0} * b{1} = {2}", a.ToString(), b.ToString(), a.SubScalar(b));
+            Console.WriteLine("a{0} * b{1} = {2}", a.ToString(), b.ToString(), a * b);
+
+           // Console.ReadKey();
+
+            Console.WriteLine("Задание 2.Многочлены");
+            Polynomial p1 = new Polynomial(1, 15);
+            Polynomial p2 = new Polynomial(3, 12, 24, 35);
+            Console.WriteLine("{0} + {1} = {2}", p1, p2, p1 + p2);
+            Console.WriteLine("{0} - {1} = {2}", p1, p2, p1 - p2);
+            Console.WriteLine("{0} * {1} = {2}", p1, p2, p1 * p2);
+            Console.WriteLine("{0} равно {1} = {2}", p1, p2, p1 == p2);
+            Console.WriteLine("{0} и {1}: х = {2}", p1, p2, (p1 * p2).Calculate(1.2d));
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Vector.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VectorAndPolynomial
+{
+    class Vector
+    {
+        private double x;
+        public double X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        private double y;
+        public double Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        private double z;
+        public double Z
+        {
+            get { return z; }
+            set { z = value; }
+        }
+
+        // Конструктор
+        public Vector(double x, double y, double z)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
+        }
+
+        //===============================================================
+        // ---------------------ДЛИНА ВЕКТОРА----------------------------
+        //===============================================================
+
+        // Свойство
+        private double length;
+        public double Length
+        {
+            get
+            {
+                return Math.Round(Math.Sqrt(X * X + Y * Y + Z * Z), 2);
+            }
+            set
+            {
+                length = value;
+            }
+        }
+
+        //===============================================================
+        //------------------УГОЛ МЕЖДУ ДВУМЯ ВЕКТОРАМИ-------------------
+        //===============================================================
+        /* Здесь возвращается косинус угла 
+         При желании можно найти градусы и радианы */
+        public double Angle(Vector v)
+        {
+            double cos = Math.Round((X * v.X + Y * v.Y + Z * v.Z) / (this.Length * v.Length), 4);
+            return Math.Acos(cos)*180/Math.PI;
+        }
+
+        //================================================================
+        // Сложение векторов
+        //================================================================
+        public Vector Plus(Vector v)
+        {
+            double xx = X + v.X;
+            double yy = Y + v.Y;
+            double zz = Z + v.Z;
+            return new Vector(xx, yy, zz);
+        }
+        // Переопределение операции +
+        public static Vector operator +(Vector v1, Vector v2)
+        {
+            return new Vector(v1.X + v2.X, v1.Y + v2.Y, v1.Z + v2.Z);
+        }
+
+        //================================================================
+        // Вычитание векторов
+        //================================================================
+        public Vector Minus(Vector v)
+        {
+            double xx = X - v.X;
+            double yy = Y - v.Y;
+            double zz = Z - v.Z;
+            return new Vector(xx, yy, zz);
+        }
+        // Переопределение операции -
+        public static Vector operator -(Vector v1, Vector v2)
+        {
+            return new Vector(v1.X - v2.X, v1.Y - v2.Y, v1.Z - v2.Z);
+        }
+
+
+        //================================================================
+        // Умножение вектора на число
+        //================================================================
+        public Vector SubDigit(double digit)
+        {
+            return new Vector(X * digit, Y * digit, Z * digit);
+        }
+        // Переопределение операции *
+        public static Vector operator *(double digit, Vector v1)
+        {
+            return new Vector(digit * v1.X, digit * v1.Y, digit * v1.Z);
+        }
+
+        //================================================================
+        // Скалярное умножение векторов
+        //================================================================
+        public double SubScalar(Vector v)
+        {
+            return Math.Round(this.Length * v.Length * this.Angle(v), 2);
+        }
+        // Переопределение операции *
+        public static double operator *(Vector v1, Vector v2)
+        {
+            return Math.Round(v1.Length * v2.Length * v1.Angle(v2), 2);
+        }
+
+        //================================================================
+        // Векторное умножение векторов
+        //================================================================
+        public Vector SubVector(Vector v)
+        {
+            return new Vector((y * v.Z - z * v.Y), (-1) * (x * v.Z - z * v.X), (x * v.Y - y * v.X));
+        }
+
+        //================================================================
+        // Переопределение метода ToString()
+        public override string ToString()
+        {
+            return String.Format("({0}, {1}, {2})", X, Y, Z);
+        }
+
+    }
+}
